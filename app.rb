85cdee3ad710193d235e8b46b6573934515d94de
MichaelAbe <michael.k.abe@gmail.com>
2020-12-22 09:45:44 -0500

    commit tree e89df47408887cd45ed3da905be1c210da02e8ab

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Kekoanui"
+      end
+    
+      get '/hometown' do 
+        "My hometown is Honolulu"
+      end
+    
+      get '/favorite-song' do
+        "My favorite song is 'Heroes'"
+      end
+
 end
